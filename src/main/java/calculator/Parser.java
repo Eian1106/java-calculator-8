7092305110e863ed3f5b5e92dc0bb5b2838ffeaa
ChoSeongHyeon <00seong116@naver.com>
2025-10-20 14:03:05 +0900

    feat: 커스텀 구분자 추출

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
+    private static final String CUSTOM_DELIMITERS_PREFIX = "//";
     private static final String DEFAULT_CUSTOM_DELIMITERS_TEXT = "//;\\n";
     private static final String[] DEFAULT_DELIMITERS = {",", ":"};
     private static final List<String> delimiters = new ArrayList<>(List.of(DEFAULT_DELIMITERS));
@@ -13,5 +14,22 @@ public class Parser {
         boolean end = text.regionMatches(3, DEFAULT_CUSTOM_DELIMITERS_TEXT, 3, 2);
 
         return start && end;
+    }
+
+    public static String getAdditionSubstring(String text) {
+        if (hasCustomDelimiter(text)) {
+            extractCustomDelimiter(text);
+            return text.substring(DEFAULT_CUSTOM_DELIMITERS_TEXT.length());
+        }
+
+        return text;
+    }
+
+    private static void extractCustomDelimiter(String text) {
+        addDelimiter(String.valueOf(text.charAt(CUSTOM_DELIMITERS_PREFIX.length())));
+    }
+
+    public static void addDelimiter(String text) {
+        delimiters.add(text);
     }
 }
